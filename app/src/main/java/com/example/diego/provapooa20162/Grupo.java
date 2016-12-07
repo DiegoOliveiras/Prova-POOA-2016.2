98060699e14ba9d57c95f5c21873637194685bfa
Dranzzel <diegoliveiras@live.com>
2016-12-07 13:56:03 -0200

    Novas Activities e Ícones
    Novas activities adicionadas, ícones para os botões adicionados, botões
    antigos alterados

---
+++ app/src/main/java/com/example/diego/provapooa20162/Grupo.java
@@ -0,0 +1,13 @@
+package com.example.diego.provapooa20162;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Grupo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grupo);
+    }
+}
