fce4dde8d161a6fe468be4f0cf72d08f0b5b90e1
Dranzzel <diegoliveiras@live.com>
2016-12-08 18:16:15 -0200

    Pesqueno bug do Tema do Aplicativo resolvido
    O tema escolhido anteriormente não é totalmente compatível com a API 19
    que estou utilizando, fazendo com que o app não ocupasse toda a tela do
    celular.

--- app/src/main/java/com/example/diego/provapooa20162/MainActivity.java
+++ app/src/main/java/com/example/diego/provapooa20162/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.diego.provapooa20162;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
