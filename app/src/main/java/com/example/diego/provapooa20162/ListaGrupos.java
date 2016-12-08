85b1e32cdb98a492f20ce47eb56558c5cd8045eb
Dranzzel <diegoliveiras@live.com>
2016-12-08 18:44:33 -0200

    Transições entre activities
    Adicionado as transições entre a MainActivity e as telas NovaConta e
    ListaGrupos

--- app/src/main/java/com/example/diego/provapooa20162/ListaGrupos.java
+++ app/src/main/java/com/example/diego/provapooa20162/ListaGrupos.java
@@ -1,13 +1,28 @@
 package com.example.diego.provapooa20162;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ListaGrupos extends AppCompatActivity {
+
+    private ImageButton btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_grupos);
+
+        btnVoltar = (ImageButton) findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(ListaGrupos.this, MainActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/diego/provapooa20162/NovaConta.java
+++ app/src/main/java/com/example/diego/provapooa20162/NovaConta.java
@@ -1,13 +1,28 @@
 package com.example.diego.provapooa20162;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class NovaConta extends AppCompatActivity {
+
+    private ImageButton btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nova_conta);
+
+        btnVoltar = (ImageButton) findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(NovaConta.this, MainActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
