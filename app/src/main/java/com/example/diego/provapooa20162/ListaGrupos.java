20ade8afd64bbbd22a4b32e1f6adcee57d3abcb4
Dranzzel <diegoliveiras@live.com>
2016-12-11 12:48:01 -0200

    Mais mudanças!
    Ícones antigos removidos.
    Novos ícones adicionados.
    Botões Voltar removidos.
    Reprogramamos o botão Voltar do celular para navegação no App.
    Tamanho, posição e ícones de todos botões foram alterados.
    Adicionado uma imagem na tela inicial (logo do site Flaticon).
    Foi criado a activity FlaticonCredits onde é registrado que os ícones
    utilizados são de outra autoria.
    Adicionado a função ao logo na tela inicial que leva à tela de créditos.
    Adicionado a função no texto da tela de créditos que leva à página do
    autor.

--- app/src/main/java/com/example/diego/provapooa20162/ListaGrupos.java
+++ app/src/main/java/com/example/diego/provapooa20162/ListaGrupos.java
@@ -9,20 +9,15 @@ import android.widget.ImageButton;
 
 public class ListaGrupos extends AppCompatActivity {
 
-    private ImageButton btnVoltar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_grupos);
 
-        btnVoltar = (ImageButton) findViewById(R.id.btnVoltar);
-        btnVoltar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent it = new Intent(ListaGrupos.this, MainActivity.class);
-                startActivity(it);
-            }
-        });
+    }
+
+    public void onBackPressed() {
+        Intent it = new Intent(ListaGrupos.this, MainActivity.class);
+        startActivity(it);
     }
 }
